2ad6ba38ef82874fcbd3fc89f6edb3d83b639418
griffi-gh <prasol258@gmail.com>
2022-04-28 15:58:03 +0200

    fuck forgot to add the directory

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import 'dotenv/config';
 import * as fs from 'fs/promises';
 import express from 'express';
 import Api from './api.js';
-console.log("Starting...");
 
-console.log("\tConnecting to database...");
+console.log("Starting robo2-web...");
+console.log("- Connecting to database...");
+
 const api = await (new Api()).connect();
 
-console.log("\tStarting a web server...");
+console.log("- Starting the server...");
 
 function procNull(res, data) {
   if(data == null) {
@@ -18,32 +19,19 @@ function procNull(res, data) {
 }
 
 const app = express();
+app.set('trust proxy', true);
 
-app.use('/web/robo/editor', express.static('editor'));
-
+//Log requests
 app.use((req, res, next) => {
-  console.log(req.path);
+  console.log(`REQUEST ${req.ip} => ${req.method} ${req.path}`);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send(`
-    <a href="web/robo/editor/">
-      <button type="button">
-        Level editor
-      </button>
-    </a>
-    <br><hr><br>
-    Use this Robo 2 API endpoint:<br>
-    <a href="/web/robo">
-      <span id="url">/</span>web/robo 
-    </a>
-    <script type="module">
-      document.getElementById("url").textContent = window.location.href
-    </script>
-  `);
-});
+//Static
+app.use('/web/robo/editor', express.static('editor'));
+app.use('/', express.static('static'));
 
+//Robo 2 API
 app.get('/web/robo/levels/desc', async (req, res) => {
   let data = await api.roboDesc();
   procNull(res, data);
@@ -54,7 +42,8 @@ app.get('/web/robo/levels/:id', async (req, res) => {
   procNull(res, data);
 });
 
+//Start server
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Done!\nRunning on port ${port}`);
+  console.log(`Done! Running on port ${port}\n`);
 });
